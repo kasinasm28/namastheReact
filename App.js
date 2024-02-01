90d7b4ea56699a8c59e04a3a8734f91de41dcbbe
kasinasm28 <kasinadhunisriram@gmail.com>
2024-02-01 16:31:48 -0600

    Namasthe React Learning

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        [React.createElement("h1",{},"Hello World 1"), React.createElement("h2", {}, "Hello World 2")]
+    )
+)
+    // const heading= React.createElement("h1", {id: "heading", xyz:"abc"}, "Hello World From React");
+    const root= ReactDOM.createRoot(document.getElementById("root"));
+    root.render(parent);
