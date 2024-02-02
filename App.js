3f5368203cee6db0768ae91ee838a059ee99a0a7
kasinasm28 <kasinadhunisriram@gmail.com>
2024-02-01 19:23:34 -0600

    Added Parcel and other dependencies and Removed CDN

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent=React.createElement(
     "div",
     {id:"parent"},
@@ -7,6 +10,6 @@ const parent=React.createElement(
         [React.createElement("h1",{},"Hello World 1"), React.createElement("h2", {}, "Hello World 2")]
     )
 )
-    // const heading= React.createElement("h1", {id: "heading", xyz:"abc"}, "Hello World From React");
-    const root= ReactDOM.createRoot(document.getElementById("root"));
-    root.render(parent);
+// const heading= React.createElement("h1", {id: "heading", xyz:"abc"}, "Hello World From React");
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
